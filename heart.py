7dbba8bce08c2c0491236fb0320d6c496dd51862
Cédric <cedrictvg@gmail.com>
2023-02-06 09:12:03 +0100

    Added heart.py

---
+++ heart.py
@@ -0,0 +1,11 @@
+from turtle import *
+color("red", "pink") # ONLT CHANGE THIS (outline, fill)
+begin_fill()
+left(50)
+forward(100)
+circle(40, 180)
+left(260)
+circle(40, 180)
+forward(100)
+end_fill()
+done()
